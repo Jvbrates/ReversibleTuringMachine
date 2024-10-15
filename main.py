a10bf0f8b7612b90a402b3b8cec0f043d948840f
jvbrates <jv.belmonterates@gmail.com>
2024-10-15 16:27:40 -0300

    Fazendo Reader

--- main.py
+++ main.py
@@ -1,81 +1,71 @@
-from typing import List
-from enum import Enum
+"""Entrada-Quintupla.txt
+# Linha 1: número de estados, número de símbolos no alfabeto de entrada, número de símbolos no alfabeto da fita e
+número de transições, respectivamente.
+# Linha 2: Estados. O último estado representa o estado de aceitação.
+# Linha 3: alfabeto de entrada
+# Linha 4: alfabeto da fita.
+# Linhas ~: funcão de transição (como explicada no artigo).
+# Última linha: Entrada
 
-class ShiftMove(Enum):
-    NO_MOVE = "0"
-    LEFT = "+"
-    RIGHT = "-"
-    INPUT = "/"
-    NONE = None
-
-    @classmethod
-    def _missing_(cls, value):
-        for member in cls:
-            if member.value == value:
-                return member
-
-        return cls.NONE
+"""
+import re
+from re import match
+from sys import stdin
+from typing import IO
+from TuringMachines import TransactionQuintuple, ShiftMove
 
 
-class TransactionQuadruple:
-    input_state: int
-    input_symbol: str
-    output_state: int
-    symbol_or_move: str | ShiftMove
+def make_trasnaction_quintuple(input: str) -> TransactionQuintuple:
+    # Expressão regular para capturar os números e símbolos
+    padrao = r"\((\d+),([A-Z])\)=\((\d+),([A-Z]),([A-Z])\)"
 
-    def __init__(self, input_state, input_symbol, output_state, symbol_or_move):
-        if (ShiftMove(input_symbol) == ShiftMove.INPUT and ShiftMove(symbol_or_move) == ShiftMove.NONE
-                or ShiftMove(input_symbol) != ShiftMove.INPUT and ShiftMove(symbol_or_move) != ShiftMove.NONE):
-            raise Exception("Transição incongruente")
+    # Usar re.match para extrair os valores
+    match = re.match(padrao, input)
 
-        self.input_state = input_state
-        self.input_symbol = input_symbol
-        self.output_state = output_state
-        self.symbol_or_move = symbol_or_move
+    if match:
+        # Extrair os grupos correspondentes
+        input_state = match.group(1)
+        input_symbol = match.group(2)
+        output_state = match.group(3)
+        output_symbol = match.group(4)
+        move = ShiftMove(match.group(5))
+    # 
+    return TransactionQuintuple(input_state, input_symbol, output_state, output_symbol, move)
+
+# TODO: Faria mais sentido matemático, usar set() ao invés de list()
+def turing_from_file(file: IO):
+
+    # Linha 1: Informações de Controle
+    n_states, n_input_symbols, n_tape_symbols, n_trans = file.readline().split(sep=' ')
+    print(n_states, n_input_symbols, n_tape_symbols, n_trans)
+
+    # Linha 2: Estados
+    states = file.readline().split(sep=' ')
+    print(states)
+    if len(states) != n_states:
+        raise Exception("Linha 2: Número de estados informado não corresponde ao fornecido")
+    accept_state = states[-1]
+
+    # Linha 3:
+    input_symbols = file.readline().split(sep=' ')
+    if len(input_symbols) != n_input_symbols:
+        raise Exception("Linha 3: Número de simbolos de entrada informado não corresponde ao fornecido")
+
+    # Linha 4
+    input_tape = file.readline().split(sep=' ')
+    if len(input_tape) != n_tape_symbols:
+        raise Exception("Linha 4: Número de simbolos de fita informado não corresponde ao fornecido")
+    # TODO: Poderia verificar que o conjunto de simbolos de entrada é subset do conjunto de simbolos da fita
+
+    # Transições
+    remain_lines = file.readlines()
+
+    transactions = []
+    for line in remain_lines[0:-1]:
+        transactions.append(make_trasnaction_quintuple(line))
+    if len(transactions) != n_trans:
+        raise Exception("Número de transições não corresponde ao fornecido")
 
 
-class TransactionQuintuple:
-    # accept_state: bool
-    input_state: int
-    input_symbol: str
-    output_state: int
-    output_symbol: int
-    move: ShiftMove
 
-    def __init__(self, input_state, input_symbol, output_state, output_symbol, move):
-        self.input_state = input_state
-        self.input_symbol = input_symbol
-        self.output_state = output_state
-        self.output_symbol = output_symbol
-        self.move = move
-
-    def getQuadruple(self, displacement=0) -> [TransactionQuadruple, TransactionQuadruple]:
-        q1 = TransactionQuadruple(self.input_state, self.input_symbol, self.input_state+displacement, self.output_symbol)
-        q2 = TransactionQuadruple(q1.input_state, ShiftMove.INPUT.value, self.output_state, self.move)
-        return q1, q2
-
-
-class StandarTuringMachine:
-    current_state: int = 0
-    tape_index: int = 0
-    transactions: List[TransactionQuintuple]
-
-    def __init__(self, transactions=None):
-        if transactions is None:
-            transactions = []
-        self.transactions = transactions
-
-    def valide(self) -> bool:
-        """ 3 ) Special quintuples: The machine includes the following quintuples
-            A1, b -> A2, b, RIGHT
-            Af-1, b -> Af, b, NO_MOVE
-
-            These two are thus thefirst and last executed respectively
-            in any terminating computation on a standard input. The
-            letter b represents a blank.
-
-            - buscar o primeiro estado e ver que ele só aparece uma vez
-            - buscar o ultimo estado e ver que ele só aparece uma vez
-            """
-
-        return True
+turing_from_file(stdin)
